32e8d60bd6f9d4f0c7d4fa6e811cc3a81f0a7111
SheddyJnr <rickyman.ss18@gmail.com>
2024-11-29 20:56:36 +0200

    app name change

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -20,7 +20,7 @@ export default function Welcome({
 
     return (
         <>
-            <Head title={process.env.APP_NAME || 'Welcome'} />
+            <Head title={env.APP_NAME} />
             <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
                 <img
                     id="background"
