6bd9d377b6ca1f6f09b894f89c704bd3f3042435
SheddyJnr <rickyman.ss18@gmail.com>
2024-11-29 20:33:46 +0200

    venue updated on card

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -23,12 +23,12 @@ export default function Welcome({
             <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
                 <img
                     id="background"
-                    className="absolute top-0 max-w-[100%] opacity-75"
+                    className="absolute top-0 w-full opacity-75"
                     src="/assets/bg_1_7.jpg"
                 />
                 <div className="relative flex min-h-screen flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
-                    <div className="relative max-w-xl px-6 h-1/3">
-                        <div className="flex flex-col items-center mt-5 justify-center gap-4 rounded-lg bg-white p-3 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800">
+                    <div className="relative w-full px-4 h-1/3 sm:max-w-xl sm:px-6">
+                        <div className="flex flex-col items-center mt-5 justify-center gap-4 rounded-lg bg-white p-3 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] dark:bg-zinc-900 dark:ring-zinc-800 sm:pb-10">
                             <img
                                 id=""
                                 className="w-full max-w-ms h-auto rounded-lg"
