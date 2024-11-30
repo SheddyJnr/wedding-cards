78b4f11c6e4d1f7ac6abc78b0e001769c9db0444
SheddyJnr <rickyman.ss18@gmail.com>
2024-11-30 21:05:19 +0200

    app name

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -19,7 +19,7 @@ export default function Welcome({
 
     return (
         <>
-            <Head title={process.env.APP_NAME} />
+            <Head title={""} />
             <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
                 <img
                     id="background"
