d6e1d57308c01d699404fde178ee8ddde7a5854e
Shadrick Siwakwi <SSiwakwi@napsa.co.zm>
2024-12-03 16:00:17 +0200

    registernandg

--- routes/auth.php
+++ routes/auth.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
+    Route::get('registernandg', [RegisteredUserController::class, 'create'])
         ->name('registernandg');
 
     Route::post('registenandg', [RegisteredUserController::class, 'store']);
